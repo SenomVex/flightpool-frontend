520817d7fb5829faa4c702370890d48389629dda
SenomVex <elemtronnajapo4ta@gmail.com>
2025-02-11 11:02:30 +0700

    Add files via upload

--- flightpool-frontend-main/scripts/index.js
+++ flightpool-frontend-main/scripts/index.js
@@ -1,12 +1,16 @@
-const themeBtn =document.querySelector('.theme');
-console.log('themeBtn');
-themeBtn.addEventListener('.click', function(){
-   const currentTheme = document.body.classList[0];
-    if (currentTheme == 'dark') {
+
+const themeBtn = document.querySelector('.theme');
+
+themeBtn.addEventListener('click', function(){
+    
+    const currentTheme = document.body.classList[0];
+
+    if(currentTheme == 'dark'){
         document.body.classList.remove('dark')
         document.body.classList.add('light')
-    }else {
-document.body.classList.remove('light');
-document.body.classList.add('dark');
+    }else{
+        document.body.classList.remove('light')
+        document.body.classList.add('dark')
     }
+
 });
